4104f6ad7c56680a39780d7de2b36a76129fd653
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-24 22:55:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/trans51665165 /LazyDeveloper
+git clone https://github.com/konichiwa55115/trans45161565 /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 cd src
